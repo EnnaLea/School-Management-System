2d1752ad4b9868364368b56363b7318fd9048041
EnnaLea <49825624+leaaliman@users.noreply.github.com>
2022-09-13 21:04:40 +0200

    change4
    creation of the main class

---
+++ src/school/managment/system/Main.java
@@ -0,0 +1,30 @@
+package school.managment.system;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Teacher lizzy = new Teacher(1,  "Lizzy", 500);
+        Teacher melissa = new Teacher(2,  "Melissa", 700);
+        Teacher paolo = new Teacher(3,  "Paolo", 600);
+
+        List<Teacher> teacherList = new ArrayList<>();
+        teacherList.add(lizzy);
+        teacherList.add(melissa);
+        teacherList.add(paolo); 
+
+        Student tamara = new Student(1, "Tamara", 4);
+        Student bill = new Student(2, "Bill", 5);
+        Student luca = new Student(3, "Luca", 3);
+
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(tamara);
+        studentList.add(bill);
+        studentList.add(luca);
+
+        School ghs = new School(teacherList, studentList);
+        System.out.println("GHS has earned " + ghs.getTotalMoneyEarned());
+
+    }
+}
